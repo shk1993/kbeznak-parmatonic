232437a3411bf60b1d842bb75d56804498da1eae
Shashwat Kumar <shashwat.kumar.2007@gmail.com>
2018-02-20 16:27:25 -0600

    Create kbeznak parmatonic.java

---
+++ kbeznak parmatonic.java
@@ -0,0 +1,9 @@
+int kbeznak = 0 ;
+int parmatonic =0;
+String kbeznak="";
+String parmatonic="";
+String x = "parmatonic kbeznak"
+
+public static void kbeznak-parmatonic(){
+  System.out.print("kbeznak-parmatonic");
+}
