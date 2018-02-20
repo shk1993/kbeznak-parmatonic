30d005fd9f7153961b2fdc26e39beeb5a60e43f0
Shashwat Kumar <shashwat.kumar.2007@gmail.com>
2018-02-20 16:30:10 -0600

    Create kbeznak parmatonic.js

---
+++ kbeznak parmatonic.js
@@ -0,0 +1,9 @@
+let kbeznak = 0 ;
+let parmatonic =0;
+let kbeznak="";
+let parmatonic="";
+let x = "parmatonic kbeznak"
+
+function kbeznak-parmatonic(){
+  System.out.print("kbeznak-parmatonic");
+}
