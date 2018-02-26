feef74692d0f3fde49a931ce43d7b286385eb318
Shashwat Kumar <shashwat.kumar.2007@gmail.com>
2018-02-26 00:11:37 -0600

    Update kbeznak parmatonic.java

--- kbeznak parmatonic.java
+++ kbeznak parmatonic.java
@@ -3,6 +3,10 @@ int parmatonic =0;
 String kbeznak="";
 String parmatonic="";
 String x = "parmatonic kbeznak"
+  
+  /*
+  With Osiris, Amun-Ra is the most widely recorded of the Egyptian gods. As the chief deity of the Egyptian Empire, Amun-Ra also came to be worshipped outside Egypt, according to the testimony of ancient Greek historiographers in Libya and 
+  Nubia. As Zeus Ammon he came to be identified with Zeus in Greece.*/
 
 public static void kbeznak-parmatonic(){
   System.out.print("kbeznak-parmatonic");
